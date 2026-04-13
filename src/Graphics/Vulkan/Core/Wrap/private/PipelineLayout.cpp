08885f4d9af2c5c6e75f7d11ea7ecd6d3e5e7a07
Egor <iiconn@mail.ru>
2026-04-13 16:17:40 +0300

    added pipeline layout handler

---
+++ src/Graphics/Vulkan/Core/Wrap/private/PipelineLayout.cpp
@@ -0,0 +1,29 @@
+//
+// Created by igor on 4/13/26.
+//
+
+#include "PipelineLayout.hpp"
+#include "Message.hpp"
+
+#include <stdexcept>
+
+namespace crv::graphics::vulkan {
+    PipelineLayout::PipelineLayout(const PipelineLayoutCreateInfo &info): mDevice(info.device) {
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+            .setLayoutCount = 1,
+            .pSetLayouts = info.layouts.data(),
+            .pushConstantRangeCount = 0,
+            .pPushConstantRanges = nullptr
+    };
+
+        if (vkCreatePipelineLayout(mDevice, &pipelineLayoutInfo, nullptr, &mHandle) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create pipeline layout!");
+        }
+        INFO << "Created pipeline layout!";
+    }
+
+    void PipelineLayout::destroy() {
+        vkDestroyPipelineLayout(mDevice, mHandle, nullptr);
+    }
+}
---
+++ src/Graphics/Vulkan/Core/Wrap/public/PipelineLayout.hpp
@@ -0,0 +1,28 @@
+//
+// Created by igor on 4/13/26.
+//
+
+#ifndef COLLECTION_PIPELINELAYOUT_HPP
+#define COLLECTION_PIPELINELAYOUT_HPP
+
+#include "DefaultWrapper.hpp"
+
+namespace crv::graphics::vulkan {
+    struct PipelineLayoutCreateInfo {
+        VkDevice device = VK_NULL_HANDLE;
+        std::vector<VkDescriptorSetLayout> layouts{};
+    };
+
+    class PipelineLayout: public DefaultWrapper<VkPipelineLayout> {
+    public:
+        using DefaultWrapper::DefaultWrapper;
+        explicit PipelineLayout(const PipelineLayoutCreateInfo& info);
+        PipelineLayout& operator=(PipelineLayout&&) = default;
+        ~PipelineLayout() override { PipelineLayout::destroy(); }
+        void destroy() override;
+    protected:
+        VkDevice mDevice = VK_NULL_HANDLE;
+    };
+}
+
+#endif //COLLECTION_PIPELINELAYOUT_HPP
