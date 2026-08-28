1825114fbe7a6edc72cd82c2d1fd2e72e6bdecc0
Egor <iiconn@mail.ru>
2026-08-28 17:07:44 +0300

    refactor assets dir

--- src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
+++ src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
@@ -8,7 +8,7 @@ namespace cvk = crv::graphics::vulkan;
 
 int main() {
     const cvk::PathTracerAppCreateInfo createInfo {
-        .scenePath = ASSETS_PATH"canelle_new.json",
+        .scenePath = ASSETS_PATH"sponza.json",
     };
     cvk::PathTracerApp app(createInfo);
     app.run();
