4010a21d30256efbb91f8e84aedffe22b2d710ff
Egor <iiconn@mail.ru>
2026-09-13 21:27:05 +0300

    changed scenes

--- src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
+++ src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
@@ -8,7 +8,7 @@ namespace cvk = crv::graphics::vulkan;
 
 int main() {
     const cvk::PathTracerAppCreateInfo createInfo {
-        .scenePath = SCENES_PATH"glass_test.json",
+        .scenePath = SCENES_PATH"canelle.json",
     };
     cvk::PathTracerApp app(createInfo);
     app.run();
