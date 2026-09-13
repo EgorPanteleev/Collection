23314ba29ee0c6ade7649f706b674b01945bd881
Egor <iiconn@mail.ru>
2026-09-13 15:27:04 +0300

    refactor

--- src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
+++ src/Graphics/Vulkan/PathTracer/applications/PathTracer.cpp
@@ -8,7 +8,7 @@ namespace cvk = crv::graphics::vulkan;
 
 int main() {
     const cvk::PathTracerAppCreateInfo createInfo {
-        .scenePath = SCENES_PATH"ford.json",
+        .scenePath = SCENES_PATH"metal_roughness_test.json",
     };
     cvk::PathTracerApp app(createInfo);
     app.run();
